d149b0daf5e9b29a3d9f9a97370719a0f2ae43f2
Rye-Guy <ryanracioppo95@gmail.com>
2018-08-24 18:50:50 -0400

    video list created (need to add API data)

---
+++ src/components/videoListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const VideoListItem = ({video}) =>{
+    console.log(video);
+    return (
+        <li>
+            <div className="list-group-item">
+                <div className="video-list media">
+                  <div className="media-left">
+                      <img className="media-object"></img>
+                    </div>
+                  <div className="media-body">
+                 <div className="media-heading"></div>   
+                 </div>
+                 </div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
