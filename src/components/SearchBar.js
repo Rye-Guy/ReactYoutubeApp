96e6bf33be5da5a4e8c7e503b0c9897636cd6467
Rye-Guy <ryanracioppo95@gmail.com>
2018-08-24 18:15:28 -0400

    module 2 work

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+//import react pull off the Component amd store it in Component
+import React, {Component} from 'react';
+
+
+class SearchBar extends Component{
+    
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+    }
+
+    render(){
+        //using setState allows us to maintain continutity. while react does a shit tone with the state Object
+        return (
+        <div>
+            <input onChange={(event) => this.setState({term: event.target.value})} value={this.state.term}>
+            
+            </input>
+            {/* Value of the Input: {this.state.term} */}
+        </div>
+        );
+    };
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+const API_KEY = 'AIzaSyCNXTuxT3aw_2AfPhk3VW2ibdJz9IUG8Es';
+
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/videoList';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component that generates some HTML
+// After we create it we need to make sure it gets put in the DOM.
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component{
+  constructor(props){
+      super(props)
+      
+      this.state = {videos: []};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'skateboards'}, (videos) =>{
+        this.setState({videos});
+    });
+  }
+  
+    render(){
+        return (
+            <div>
+                <SearchBar></SearchBar>
+                <VideoList videos={this.state.videos}></VideoList>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
